pikondrej/toolchains-builder

commit e60c3cc606338fa6a51c111d7d7b9e02be39149b

Update the toolchain to gcc8 and uclibc 2.38
We need to change the reference buildroot in order to use
the latest uclibc that is not affected by malloc cve.
The new toolchain should contains in the name the buildroot
version as a convention, so update the script to in order
to use the right buildroot tag

Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

build.sh
@@ -34,13 +34,13 @@ if [ ! -d "./buildroot" ]; then
 	git clone https://github.com/bootlin/buildroot-toolchains.git buildroot
 fi
 
-if [ -f "$1"--uclibc-backtrace--stable-2018.11-1.tar.bz2 ]; then
-	rm "$1"--uclibc-backtrace--stable-2018.11-1.tar.bz2
+if [ -f "$1"--uclibc-backtrace--stable-2021.05.tar.bz2 ]; then
+	rm "$1"--uclibc-backtrace--stable-2021.05.tar.bz2
 fi
 
 cd buildroot
 git clean -fdx
-git checkout toolchains.bootlin.com-stable-2018.11-1
+git checkout toolchains.bootlin.com-stable-2021.5
 
 cat ../"$1"-uclibc-config > ./.config
 cp ../uclibc-backtrace.fragment ./
@@ -50,5 +50,5 @@ make clean
 make
 make sdk
 
-cp ../"$1"-readme.txt ./"$1"--uclibc--stable-2018.11-1
-tar -cjf ../"$1"--uclibc-backtrace--stable-2018.11-1.tar.bz2 "$1"--uclibc--stable-2018.11-1
+cp ../"$1"-readme.txt ./"$1"--uclibc--stable-2021.05
+tar -cjf ../"$1"--uclibc-backtrace--stable-2021.05.tar.bz2 "$1"--uclibc--stable-2021.5

armv5-eabi-uclibc-config
@@ -1,10 +1,11 @@
 BR2_arm=y
 BR2_WGET="wget --passive-ftp -nd -t 3 --no-check-certificate"
-BR2_HOST_DIR="$(TOPDIR)/armv5-eabi--uclibc--stable-2018.11-1"
+BR2_HOST_DIR="$(TOPDIR)/armv5-eabi--uclibc--stable-2021.05"
 BR2_GNU_MIRROR="http://ftp.gnu.org/pub/gnu"
-BR2_KERNEL_HEADERS_4_1=y
+BR2_KERNEL_HEADERS_4_19=y
 BR2_TOOLCHAIN_BUILDROOT_LOCALE=y
 BR2_PTHREAD_DEBUG=y
+BR2_GCC_VERSION_8_X=y
 BR2_TOOLCHAIN_BUILDROOT_CXX=y
 BR2_UCLIBC_CONFIG_FRAGMENT_FILES="$(TOPDIR)/uclibc-backtrace.fragment"
 BR2_PACKAGE_HOST_GDB=y

armv7-eabihf-uclibc-config
@@ -2,11 +2,12 @@ BR2_arm=y
 BR2_cortex_a9=y
 BR2_ARM_ENABLE_VFP=y
 BR2_WGET="wget --passive-ftp -nd -t 3 --no-check-certificate"
-BR2_HOST_DIR="$(TOPDIR)/armv7-eabihf--uclibc--stable-2018.11-1"
+BR2_HOST_DIR="$(TOPDIR)/armv7-eabihf--uclibc--stable-2021.05"
 BR2_GNU_MIRROR="http://ftp.gnu.org/pub/gnu"
-BR2_KERNEL_HEADERS_4_1=y
+BR2_KERNEL_HEADERS_4_19=y
 BR2_TOOLCHAIN_BUILDROOT_LOCALE=y
 BR2_PTHREAD_DEBUG=y
+BR2_GCC_VERSION_8_X=y
 BR2_TOOLCHAIN_BUILDROOT_CXX=y
 BR2_UCLIBC_CONFIG_FRAGMENT_FILES="$(TOPDIR)/uclibc-backtrace.fragment"
 BR2_PACKAGE_HOST_GDB=y

passgat/wh-toolchains-builder

commit 3b5da867a81f9d9bdb7d3647e12535623f2af1e9

Fix creation of 'armv7-eabihf' toolchain
This patch does not change any configuration from commit
e60c3cc60633 ("Update the toolchain to gcc8 and uclibc 2.38").

It fixes the tolchain creation script and updates the versions of
the software packages used that still refer to version 2018.11-1.

Note: The uclibc version is 1.0.38 and not 2.38.

Jira-Id: WAN-831
Fixes: e60c3cc60633 ("Update the toolchain to gcc8 and uclibc 2.38")
Signed-off-by: Dario Binacchi <dario.binacchi@amarulasolutions.com>

build.sh
@@ -41,7 +41,7 @@ fi
 
 cd buildroot
 git clean -fdx
-git checkout toolchains.bootlin.com-stable-2021.5
+git checkout toolchains.bootlin.com-2021.05
 
 cat ../"$1"-uclibc-config > ./.config
 cp ../uclibc-backtrace.fragment ./
@@ -52,4 +52,4 @@ make
 make sdk
 
 cp ../"$1"-readme.txt ./"$1"--uclibc--stable-2021.05
-tar -cjf ../"$1"--uclibc-backtrace--stable-2021.05.tar.bz2 "$1"--uclibc--stable-2021.5
+tar -cjf ../"$1"--uclibc-backtrace--stable-2021.05.tar.bz2 "$1"--uclibc--stable-2021.05